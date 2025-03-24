72afb6c4966290012bc82d665ec572d59f464113
Jose Galvez <jlgalvez77@gmail.com>
2025-03-24 01:16:24 +0100

    24/03/25- madrugada

---
+++ colecciones/listas.py
@@ -0,0 +1,21 @@
+print('*** Listas ***')
+
+mi_lista = [1, 2, 3, 4, 5]
+
+# Acceder a un elemento por su indice
+print(mi_lista[4])
+
+# Modificar un elemento de la lista
+mi_lista[1] = 10
+print(mi_lista)
+
+# Agregar un nuevo elemento a la lista
+mi_lista.append(6)
+print(mi_lista)
+
+# Eliminar elementos de la lista
+del mi_lista[2]
+print(mi_lista)
+
+# Saber la longitud de la lista
+print(len(mi_lista))
